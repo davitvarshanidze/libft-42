ce09195d9dd88468b1362e141ca0d3b7c82609f7
datodotio <datodotio@gmail.com>
2024-08-31 18:12:20 +0400

    cc

--- libft.h
+++ libft.h
@@ -15,4 +15,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void *memset(void *s, int c, size_t n);
+void bzero(void *s, size_t len);
+
 #endif
---
+++ test.c
@@ -0,0 +1,7 @@
+#include "libft.h"
+
+int main() {
+  char buffer[50];
+  memset(buffer, 0, sizeof(buffer));
+  return 0;
+}
