97f1666d5fe68d91b8f9fcb61f6ed3a9c21b6139
datodotio <datodotio@gmail.com>
2024-09-01 21:49:34 +0400

    cc

--- ft_memset.c
+++ ft_memset.c
@@ -1,13 +1,11 @@
 #include "libft.h"
 
-void *ft_memset(void *b, int c, size_t len)
-{
-    char *p;
-    p = (char *)b;
-    while (len > 0)
-    {
-        p[len - 1] = c;
-        len--;
-    }
-    return (b);
+void *ft_memset(void *b, int c, size_t len) {
+  char *p;
+  p = (char *)b;
+  while (len > 0) {
+    p[len - 1] = c;
+    len--;
+  }
+  return (b);
 }
