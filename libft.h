0bc62898449c5c4e671c3a7318f7c86cdc94f8d2
datodotio <datodotio@gmail.com>
2024-09-02 15:43:59 +0400

    cc

--- libft.h
+++ libft.h
@@ -3,7 +3,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void *memset(void *s, int c, size_t n);
+#define BUFF_SIZE 100
+#define MALLCHECK(x) if (!x) return (-1);
+#define IS_SPACE(x) (x == ' ' || x == '\t' || x == '\r' || x == '\f')
+
+void *memset(void *s, int c, size_t len);
 void bzero(void *s, size_t n);
 
 #endif
