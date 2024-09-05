000bfdefcb7760ff56c1b974eed2cb149a295aa6
datodotio <datodotio@gmail.com>
2024-09-05 18:26:27 +0400

    cc

--- libft.h
+++ libft.h
@@ -3,12 +3,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define BUFF_SIZE 100
-#define MALLCHECK(x) \
-  if (!x)            \
-    return (-1);
-#define IS_SPACE(x) (x == ' ' || x == '\t' || x == '\r' || x == '\f')
-
 void *ft_memset(void *s, int c, size_t len);
 void *ft_bzero(void *s, size_t n);
 void *ft_memcpy(void *dst, const void *src, size_t n);
