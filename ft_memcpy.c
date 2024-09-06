3519cce869dfc56607822d01f2f0bfe2753f9a3c
datodotio <datodotio@gmail.com>
2024-09-06 19:22:34 +0400

    adding memccpy function

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -2,11 +2,10 @@
 
 void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-	char *d = (char *)dest;
-	const char *s = (const char*) src;
-
-	for (size_t i = 0; i < n; i++) {
-		d[i] = s[i];
+	char *d = dst;
+	const char *s = src;
+	while (n--) {
+		*d++ = *s++;
 	}
 	return dest;
 }
