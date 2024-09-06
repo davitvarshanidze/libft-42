011070f40ed34d385ba1129ba0db66d17474daf6
datodotio <datodotio@gmail.com>
2024-09-06 16:35:27 +0400

    cc

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,3 +1,13 @@
 #include "libft.h"
 
 void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	char *d = (char *)dest;
+	const char *s = (const char*) src;
+
+	for (size_t i = 0; i < n; i++) {
+		d[i] = s[i];
+	}
+	return dest;
+}
+
