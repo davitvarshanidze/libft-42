410cf1370967461dfb7e8706f13b9263efa6c8c1
davidvarshanidze <dvarsshanidze@gmail.com>
2024-09-09 00:28:38 +0400

    batman

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,3 +1,14 @@
 #include "libft.h"
 
-void *ft_memcmp(const void *s1, const void *s2, size_t n);
+void *ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char *src1 = (const unsigned char *)s1;
+	const unsigned char *src2 = (const unsigned char *)s2;
+	for (size_t i = 0; i < n; i++) {
+		if (src1[i] != src2[i]) {
+			return src1[i] - src2[i];
+		}
+	}
+	return 0;
+}
+
