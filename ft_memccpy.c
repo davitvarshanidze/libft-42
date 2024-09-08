ff5b2b5422e2288e9661fcba4ac5171bc0a8d103
davidvarshanidze <dvarsshanidze@gmail.com>
2024-09-08 19:04:09 +0400

    batman

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,6 +1,15 @@
 #include "libft.h"
 
-void *ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
+void *memccpy(void *dest, const void *src, int c, size_t n)
 {
-
+	unsigned char *d = (unsigned char *)dest;
+	const unsigned char *s =  (const unsigned char *)src;
+	unsigned char ch = (unsigned char)c;
+	for (size_t i = 0; i < n; i++) {
+		d[i] = s[i];
+		if (s[i] == ch) {
+			return (void *)(d + i + 1);
+		}
+	}
+	return NULL;
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 void *ft_memset(void *b, int c, size_t len);
 void *ft_bzero(void *s, size_t n);
 void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
-void *ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
+void *memccpy(void *dest, const void *src, int c, size_t n);
 void *ft_memmove(void *dst, const void *src, size_t len);
 void *ft_memchr(const void *s, int c, size_t n);
 
