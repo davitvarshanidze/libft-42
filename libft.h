07d91575833c81404ca1c21dae33db29695469fe
davidvarshanidze <dvarsshanidze@gmail.com>
2024-09-09 19:44:17 +0400

    batman

--- libft.h
+++ libft.h
@@ -18,5 +18,6 @@ void *ft_strrchr(const char *s, int c);
 void *ft_strnstr(const char *big, const char *little, size_t len);
 void *ft_strncmp(const char *s1, const char *s2, size_t n);
 void *ft_atoi(const char *str);
+void *ft_isalpha(int c);
 
 #endif
